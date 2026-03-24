511daa2cec6381f464194422be803a10c8edab23
Stephen Huan <stephen.huan@cgdct.moe>
2026-03-24 05:39:13 -0400

    build(flake.nix): add flake

---
+++ flake.nix
@@ -0,0 +1,81 @@
+{
+  description = "Parallelized BBFMM3D with OpenMP";
+
+  inputs = {
+    nixpkgs.follows = "maipkgs/nixpkgs";
+    maipkgs.url = "github:stephen-huan/maipkgs";
+  };
+
+  outputs = { self, nixpkgs, maipkgs }:
+    let
+      inherit (nixpkgs) lib;
+      systems = lib.systems.flakeExposed;
+      eachDefaultSystem = f: builtins.foldl' lib.attrsets.recursiveUpdate { }
+        (map f systems);
+    in
+    eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) python3Packages;
+        inherit (maipkgs.legacyPackages.${system}) python;
+        python' = python.withPackages (ps: with ps; [
+          build
+          gpjax
+          matplotlib
+          numpy
+          scikit-learn
+          scipy
+          self.packages.${system}.default
+          setuptools
+        ]);
+        formatters = [ pkgs.black pkgs.isort pkgs.nixpkgs-fmt ];
+        linters = [ pkgs.pyright pkgs.ruff pkgs.statix ];
+      in
+      {
+        packages.${system} = {
+          default = (maipkgs.packages.${system}.pbbfmm3d.overrideAttrs {
+            src = ./.;
+          }).override {
+            withMkl = true;
+          };
+        };
+
+        formatter.${system} = pkgs.writeShellApplication {
+          name = "formatter";
+          runtimeInputs = formatters;
+          text = ''
+            isort "$@"
+            black "$@"
+            nixpkgs-fmt "$@"
+          '';
+        };
+
+        checks.${system}.lint = pkgs.stdenvNoCC.mkDerivation {
+          name = "lint";
+          src = ./.;
+          doCheck = true;
+          nativeCheckInputs = lib.singleton python' ++ formatters ++ linters;
+          checkPhase = ''
+            isort --check --diff .
+            black --check --diff .
+            nixpkgs-fmt --check .
+            ruff check .
+            pyright .
+            statix check
+          '';
+          installPhase = "touch $out";
+        };
+
+        devShells.${system}.default = pkgs.mkShell {
+          packages = [
+            python'
+            pkgs.fftw
+            pkgs.mkl
+            python3Packages.boost
+          ]
+          ++ formatters
+          ++ linters;
+        };
+      }
+    );
+}
